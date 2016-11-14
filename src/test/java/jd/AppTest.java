6b417610309b784ec67174f0d0a2c80f103dc27e
zhaoguoping <zhaoguoping@jd.com>
2016-11-14 13:33:14 +0800

    AppTest 增加case

--- src/test/java/jd/AppTest.java
+++ src/test/java/jd/AppTest.java
@@ -1,18 +1,33 @@
 package jd;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.ITestContext;
-import org.testng.annotations.*;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    @Test
+    public void test01(){
+        Employee harry = new Employee("Harry Hacker", 1000, 2016, 11, 13);
+        harry.raiseSalary(20);
+        System.out.println("name" + harry.getName() + ", salary=" + harry.getSalary());
+    }
+
+    @Test
+    public void test02(){
+        Employee[] staff = new Employee[3];
+        Manager boss = new Manager("cui yan", 2000, 2013, 3, 4);
+        boss.setBonus(500);
+        //fill the staff with Manager and  Employee objects
+        staff[0] = boss;
+        staff[1] = new Employee("zhao guoping", 18000, 2016, 8, 8);
+        staff[2] = new Employee("cui junling", 12000, 2011, 8, 1);
+        //print out information about all Employee objects
+        for (Employee e : staff){
+            System.out.println("name = "+e.getName()+", salary = "+e.getSalary());
+        }
+    }
 
 }
 
