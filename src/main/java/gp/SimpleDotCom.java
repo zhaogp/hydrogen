5cfb0920769c85ae0fbddd34d9c3f00e9f3ba1e4
zhaoguoping <gp163_zhao@163.com>
2017-10-15 14:00:45 +0800

    Use apache log4j2 logging system

--- src/main/java/gp/SimpleDotCom.java
+++ src/main/java/gp/SimpleDotCom.java
@@ -1,14 +1,37 @@
 package gp;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import java.util.Arrays;
+
 public class SimpleDotCom {
+    public static final Logger logger = LogManager.getLogger(SimpleDotCom.class);
     private int[] locationCells;
     private int numOfHits = 0;
 
     void setLocationCells(int[] cells){
+        logger.info("Set locations for " + Arrays.toString(cells));
+        locationCells = cells;
 
     }
-    String checkGuess(String guess){
-        return null;
+    String checkGuess(String stringGuess){
+        logger.info("Your guess is " + stringGuess);
+        int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for(int cell: locationCells){
+            if(cell == guess){
+                result = "hit";
+//                logger.info("hit");
+                numOfHits ++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length){
+//            logger.info("kill");
+            result = "kill";
+        }
+        logger.info(result);
+        return result;
     }
 
 }
--- src/test/java/gp/SimpleDotComTest.java
+++ src/test/java/gp/SimpleDotComTest.java
@@ -1,8 +1,15 @@
 package gp;
 
 import org.testng.annotations.Test;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Arrays;
+
 
 public class SimpleDotComTest {
+    public static final Logger logger = LogManager.getLogger(SimpleDotComTest.class);
+
     @Test(testName = "test hit", description = "测试猜中单元格")
     public void testGuessHit(){
         SimpleDotCom sdc = new SimpleDotCom();
@@ -13,12 +20,16 @@ public class SimpleDotComTest {
 
         // test miss
         guessResult = sdc.checkGuess("0");
+
         assert guessResult.equals("miss");
         // test hit
         guessResult = sdc.checkGuess("2");
         assert guessResult.equals("hit");
-
+        guessResult = sdc.checkGuess("3");
+        assert guessResult.equals("hit");
         // test kill
+        guessResult = sdc.checkGuess("4");
+        assert guessResult.equals("kill");
 
     }
 }
